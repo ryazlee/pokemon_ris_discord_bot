150c395f3e9b25d6b08aa83a8d31cee6e5631b27
Ryan Lee <ryan_j_lee@apple.com>
2020-11-21 18:03:15 -0800

    Update bot to reverse image search pokemons

--- discord-bot.py
+++ discord-bot.py
@@ -1,7 +1,18 @@
 import discord
 import time
+from bs4 import BeautifulSoup
+import re
+import requests
 
 client = discord.Client()
+
+def search_pokemon(url):
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    r = requests.get('http://www.google.com/searchbyimage?image_url=' + url, headers=headers)
+    resp = re.findall(r'wiki\/([a-zA-Z]+)|pokemon\+(\w+)|picuki.com\/tag\/(\w+)|gramho.com\/explore-hashtag\/(\w+)', r.text)
+    flattened_resp = list(sum(resp, ()))
+    return set(flattened_resp)
 
 @client.event
 async def on_ready():
@@ -12,13 +23,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.channel.name == 'pokemon':
-        for i in range(20):
-            await message.channel.send("this is text for poketwo")
-            time.sleep(3)
-
+    if message.content.startswith('$rb'):
+        data = message.content.split()
+        potential_pokemon = search_pokemon(data[1])
+        pp_str = ", ".join(str(p) for p in potential_pokemon if p)
+        await message.channel.send("Potential pokemon: " + pp_str)
 
 client.run('Nzc5NDg1OTI5MzQ0OTkxMjU0.X7hO1w.dFiQ_kqL4_qGqFp-kpo_AasdMBs')
