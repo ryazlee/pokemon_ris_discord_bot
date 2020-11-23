fe02d64bb356da4e36dc151265eb96320b9a15a9
Ryan Lee <ryan_j_lee@apple.com>
2020-11-23 13:19:42 -0800

    Add search query functionality

--- discord-bot.py
+++ discord-bot.py
@@ -1,17 +1,32 @@
 import discord
-import time
-from bs4 import BeautifulSoup
 import re
 import requests
 import config
+import string
 
 client = discord.Client()
 
-def search_pokemon(url):
+regex_query = r'bulbagarden.net\/wiki\/Talk:([a-zA-Z]+)|bulbagarden.net\/wiki\/([a-zA-Z]+)|picuki.com\/tag\/([a-zA-Z]+)|pokemon.fandom.com\/wiki\/([a-zA-Z]+)|pokemondb.net\/pokedex\/([a-zA-Z]+)'
+
+def query_url_creator(query):
+    query = query.lower()
+    if 'pokemon' not in query: 
+        query += 'pokemon'
+    return query.replace(string.whitespace, '+')
+
+def search_pokemon_url(url):
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
     r = requests.get('http://www.google.com/searchbyimage?image_url=' + url, headers=headers)
-    resp = re.findall(r'wiki\/([a-zA-Z]+)|pokemon\+(\w+)|picuki.com\/tag\/(\w+)|gramho.com\/explore-hashtag\/(\w+)', r.text)
+    resp = re.findall(regex_query, r.text)
+    flattened_resp = list(sum(resp, ()))
+    return set(flattened_resp)
+
+def search_pokemon_query(query):
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    r = requests.get('http://www.google.com/search?q=' + query, headers=headers)
+    resp = re.findall(regex_query, r.text)
     flattened_resp = list(sum(resp, ()))
     return set(flattened_resp)
 
@@ -24,9 +39,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$rb'):
-        data = message.content.split()
-        potential_pokemon = search_pokemon(data[1])
+    if message.content.startswith('rb'):
+        data = message.content.split('rb')
+        potential_pokemon = search_pokemon_url(data[1]) if message.content.startswith('rbu') else search_pokemon_query(query_url_creator(data[1]))
         pp_str = ", ".join(str(p) for p in potential_pokemon if p)
         await message.channel.send("Potential pokemon: " + pp_str)
 
